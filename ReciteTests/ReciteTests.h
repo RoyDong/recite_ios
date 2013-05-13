93e92510c9a77ca221f148c853b1c8662ea6aab7
Roy <roy@roys-MacBook-Pro.local>
2013-05-13 15:35:01 +0800

    init project

---
+++ ReciteTests/ReciteTests.h
@@ -0,0 +1,13 @@
+//
+//  ReciteTests.h
+//  ReciteTests
+//
+//  Created by Roy on 5/13/13.
+//  Copyright (c) 2013 Roy. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ReciteTests : SenTestCase
+
+@end
---
+++ ReciteTests/ReciteTests.m
@@ -0,0 +1,32 @@
+//
+//  ReciteTests.m
+//  ReciteTests
+//
+//  Created by Roy on 5/13/13.
+//  Copyright (c) 2013 Roy. All rights reserved.
+//
+
+#import "ReciteTests.h"
+
+@implementation ReciteTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ReciteTests");
+}
+
+@end
